03e9bf05202a7b031007abafdb168af5b41930c7
student <student@238-10.INFORMATYKA.local>
2016-10-24 09:25:27 +0200

    re-add obj Ramka

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -15,7 +15,7 @@ public class GUI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Ramka rama = new Ramka();        // TODO code application logic here
     }
     
 }
