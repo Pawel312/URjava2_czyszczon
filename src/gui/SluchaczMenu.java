c0b02c0e0f1ef92c1bbc79069de9d73e7b9dc967
DESKTOP-RABSN9K\Karol <karlkurt1996@gmail.com>
2016-11-04 13:13:23 +0100

    add label and field to frame

--- src/gui/SluchaczMenu.java
+++ src/gui/SluchaczMenu.java
@@ -5,19 +5,22 @@
  */
 package gui;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 /**
  *
  * @author student
  */
-public class SluchaczMenu implements ActionListener{
+public class SluchaczMenu extends JPanel{
     
     public SluchaczMenu(){
                 JMenuBar pasekMenu = new JMenuBar();
@@ -26,43 +29,83 @@ public class SluchaczMenu implements ActionListener{
         JLabel label2 = new JLabel("Nazwisko");
         JLabel label3 = new JLabel("Marka");
         
+        JTextField field1 = new JTextField(10);
+        JTextField field2 = new JTextField(10);
+        JTextField field3 = new JTextField(10);
         
+          setLayout(new GridBagLayout());
+        GridBagConstraints gc = new GridBagConstraints();
         pasekMenu.add(menuPliku);
         
+        gc.anchor = GridBagConstraints.LINE_END;
+        gc.weightx = 0.5;
+        gc.weighty = 0.5;
+//LABEL1
+        gc.gridx = 0;
+        gc.gridy = 0;
+
+        add(label1,gc);
+//LABEL2
+        gc.gridx = 0;
+        gc.gridy = 1;
+
+        add(label2, gc);
+ //LABEL3       
+        gc.gridx = 0;
+        gc.gridy = 2;
+
+        add(label3, gc);
+//FIELD1
+        gc.anchor = GridBagConstraints.LINE_START;
+        gc.gridx = 1;
+        gc.gridy =0;
+        add(field1,gc);
+//FIELD3
+        gc.gridx = 1;
+        gc.gridy =1;
+        add(field2,gc);
+//FIELD3
+        gc.gridx = 1;
+        gc.gridy =2;
+        add(field3,gc);
+//        gc.weighty=10;
+//        gc.anchor = GridBagConstraints.FIRST_LINE_START;
+//        gc.gridx =1;
+//        gc.gridy = 2;
+//        add(addButton,gc);
+        
         JMenuItem opcjaPolisa = new JMenuItem("Polisa");
-        opcjaPolisa.addActionListener(new SluchaczMenu());
+       // opcjaPolisa.addActionListener(new SluchaczMenu());
         menuPliku.add(opcjaPolisa);
 
         menuPliku.addSeparator();
 
         JMenuItem opcjaDrukuj = new JMenuItem("Drukuj");
-        opcjaDrukuj.addActionListener(new SluchaczMenu());
+      //  opcjaDrukuj.addActionListener(new SluchaczMenu());
         menuPliku.add(opcjaDrukuj);
 
         JMenuItem opcjaZapisz = new JMenuItem("Zapisz");
-        opcjaZapisz.addActionListener(new SluchaczMenu());
+       // opcjaZapisz.addActionListener(new SluchaczMenu());
         menuPliku.add(opcjaZapisz);
 
-        JTextField textField1 = new JTextField();
-        textField1.setHorizontalAlignment(JTextField.LEFT);
-        JTextField textField2 = new JTextField();
-        textField1.setHorizontalAlignment(JTextField.LEFT);
-        label1.setLabelFor(textField2);
 
+       
     }
 
-    public void actionPerformed(ActionEvent e) {
-        String wybrano = e.getActionCommand();
-        if (wybrano.equals("Polisa")) {
-            System.out.println("polisa");
-        } else if (wybrano.equals("Drukuj")) {
-            System.out.println("polisa");
-        } else if (wybrano.equals("Zapisz")) {
-            System.out.println("zapisanoj");}
-            else{
-                    System.exit(0);
-              //ggg
-        }
+//    public void actionPerformed(ActionEvent e) {
+//        String wybrano = e.getActionCommand();
+//        if (wybrano.equals("Polisa")) {
+//            System.out.println("polisa");
+//        } else if (wybrano.equals("Drukuj")) {
+//            System.out.println("polisa");
+//        } else if (wybrano.equals("Zapisz")) {
+//            System.out.println("zapisanoj");}
+//            else{
+//                    System.exit(0);
+//              //ggg
+//        }
 
-    }}
+//    }
+}
+
 
