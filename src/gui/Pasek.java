80f2919e7f7fb2a064d955f1151ff2e59cb83837
DESKTOP-RABSN9K\Karol <karlkurt1996@gmail.com>
2016-11-05 19:27:54 +0100

    create Pasek class

---
+++ src/gui/Pasek.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Karol
+ */
+public class Pasek extends JMenu {
+
+    public Pasek() {
+
+        JMenuBar pasekMenu = new JMenuBar();
+
+        JMenu menuPliku = new JMenu("Plik");
+        JMenuItem opcjaPolisa = new JMenuItem("Polisa");
+        menuPliku.add(opcjaPolisa);
+
+        menuPliku.addSeparator();
+
+        JMenuItem opcjaDrukuj = new JMenuItem("Drukuj");
+        menuPliku.add(opcjaDrukuj);
+
+        JMenuItem opcjaZapisz = new JMenuItem("Zapisz");
+        opcjaZapisz.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               
+            }
+        });
+        menuPliku.add(opcjaZapisz);
+        pasekMenu.add(menuPliku);
+
+        setLayout(new GridBagLayout());
+        GridBagConstraints gc = new GridBagConstraints();
+        gc.anchor = GridBagConstraints.LINE_START; 
+        gc.gridx = 0;
+        gc.gridy = 0;
+        add(pasekMenu, gc);
+    }
+}
