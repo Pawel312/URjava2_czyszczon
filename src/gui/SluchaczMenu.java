829d77d3560e74700ea954a2368c5d369903ef9d
DESKTOP-RABSN9K\Karol <karlkurt1996@gmail.com>
2016-11-06 23:11:07 +0100

    create EventListner and set raction

--- src/gui/SluchaczMenu.java
+++ src/gui/SluchaczMenu.java
@@ -10,6 +10,7 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.EventListener;
 import javax.swing.AbstractButton;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -25,11 +26,16 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
+import javax.swing.event.EventListenerList;
 import javax.swing.plaf.metal.MetalBorders;
 
-
 public class SluchaczMenu extends JPanel {
- JLabel label1 = new JLabel("imie: ");
+    
+    private EventListenerList ListenerList = new EventListenerList();
+    JTextField field1 = new JTextField(10);
+    JTextField field2 = new JTextField(10);
+    String text;
+
     public SluchaczMenu() {
         Dimension size = getPreferredSize();
         size.width = 380;
@@ -39,19 +45,18 @@ public class SluchaczMenu extends JPanel {
         setBorder(BorderFactory.createTitledBorder("dane"));
 
         JLabel label = new JLabel();
-       
+        JLabel label1 = new JLabel("Imie: ");
         JLabel label2 = new JLabel("Nazwisko: ");
         JLabel label3 = new JLabel("Marka: ");
         JLabel label4 = new JLabel("Tapicerka");
         JButton button5 = new JButton("czyść");
-      
+
         JTextField field1 = new JTextField(10);
         JTextField field2 = new JTextField(10);
 
-        String[] sList = new String[]{"Volvo", "BMW"};
+        String[] sList = new String[]{"Tesla", "Volvo", "Mercedes-Benz", "BMW", "Audi", "Daewoo"};
         JComboBox<String> seasons = new JComboBox<>(sList);
-  
-        
+
         ButtonGroup tapicerka = new ButtonGroup();
         JRadioButton[] material = new JRadioButton[2];
         material[0] = new JRadioButton("skorzana");
@@ -71,25 +76,40 @@ public class SluchaczMenu extends JPanel {
         };
         material[0].addActionListener(slActionListener);
         material[1].addActionListener(slActionListener);
-        
+
         JCheckBox jCheckBox1 = new JCheckBox("ABS");
-          JCheckBox jCheckBox2 = new JCheckBox("Klima");
-button5.addActionListener(new ActionListener() {
+        JCheckBox jCheckBox2 = new JCheckBox("Klima");
+        button5.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-               field1.setText(null);
-               field2.setText(null);
-               jCheckBox1.setSelected(false);
-               jCheckBox2.setSelected(false);
-               b1.
-               
+                String car = "";
+                String klima= "" ;
+                String abs = "";
+                String name = field1.getText();
+                String surname = field2.getText();
+                if(jCheckBox1.isSelected()){
+                    abs=("abs");
+                }
+                if(jCheckBox2.isSelected()){
+                    klima=("klima");
+                }
+                for(int i=0;i<sList.length;i++)
+                if(seasons.getSelectedIndex()==i){
+                    car = sList[i];
+                            }
+                String text = name+" "+surname+" "+car+" "+abs+" " +klima;
+                field1.setText(null);
+                field2.setText(null);
+                jCheckBox1.setSelected(false);
+                jCheckBox2.setSelected(false);
+                seasons.setSelectedIndex(0);
+                fireDetailEvent(new DetailEvent(this, text));
             }
         });
-
         setLayout(new GridBagLayout());
         GridBagConstraints gc = new GridBagConstraints();
-        
 
+        text = field2.getText();
         gc.anchor = GridBagConstraints.LINE_END;
         gc.weightx = 1.5;
         gc.weighty = 1.5;
@@ -105,7 +125,7 @@ button5.addActionListener(new ActionListener() {
 
         add(label2, gc);
         //LABEL3       
-        
+
         gc.gridx = 0;
         gc.gridy = 2;
 
@@ -117,16 +137,16 @@ button5.addActionListener(new ActionListener() {
 
         add(label4, gc);
         //checkBox1
-       gc.anchor = GridBagConstraints.FIRST_LINE_START;
-         gc.gridx = 0;
+        gc.anchor = GridBagConstraints.FIRST_LINE_START;
+        gc.gridx = 0;
         gc.gridy = 4;
 
         add(jCheckBox1, gc);
-       
+
         //checkBox2
-        gc.weighty=0.1;
+        gc.weighty = 0.1;
 //        gc.anchor = GridBagConstraints.FIRST_LINE_START;
-         gc.gridx = 1;
+        gc.gridx = 1;
         gc.gridy = 4;
 
         add(jCheckBox2, gc);
@@ -136,33 +156,39 @@ button5.addActionListener(new ActionListener() {
         gc.gridy = 0;
         add(field1, gc);
 //FIELD2
-gc.anchor = GridBagConstraints.LINE_START;
+        gc.anchor = GridBagConstraints.LINE_START;
         gc.gridx = 1;
         gc.gridy = 1;
         add(field2, gc);
 //FIELD3
-gc.anchor = GridBagConstraints.LINE_START;
+        gc.anchor = GridBagConstraints.LINE_START;
         gc.gridx = 1;
         gc.gridy = 2;
         add(seasons, gc);
- 
+
         //FIELD4
-        
-        
-        
         gc.gridx = 1;
         gc.gridy = 3;
         add(b1, gc);
 //button eta clear
-        gc.weighty=10;
+        gc.weighty = 10;
         gc.anchor = GridBagConstraints.FIRST_LINE_END;
-        gc.gridx =0;
+        gc.gridx = 0;
         gc.gridy = 6;
-        add(button5,gc);
-        
-        
-        
+        add(button5, gc);
 
     }
-    
+ public void fireDetailEvent(DetailEvent event){
+     Object[] listners = listenerList.getListenerList();
+     for(int i=0;i<listners.length;i=i+2){
+      if(listners[i]==DetailListenr.class)
+         ( (DetailListenr)listners[i+1]).detailEventO(event);
+     }
+ }
+public void addDetailListenr(DetailListenr listenr){
+    listenerList.add(DetailListenr.class, listenr);
+}
+public void removeDetailListenr(DetailListenr listenr){
+    listenerList.remove(DetailListenr.class, listenr);
+}
 }
